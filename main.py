1786506a062311a54e1730078a2e032bae521f78
ColdJustice <xblcoldjustice@gmail.com>
2025-01-14 21:13:28 -0600

    - Added gun

--- main.py
+++ main.py
@@ -1,41 +1,51 @@
+import sys
 import pygame
 
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
+from shot import Shot
 
 def main():
-    print("Starting asteroids!")
-    numpass, numfail = pygame.init()
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-
+    
     updatable = pygame.sprite.Group()
     drawable  = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
-
     asteroids = pygame.sprite.Group()
+    shots     = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
 
     asteroid_field = AsteroidField()
-
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)    
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+            
         for item in updatable:
             item.update(dt)
 
         for asteroid in asteroids:
-            if (player.collides_with(asteroid)):
+            if player.collides_with(asteroid):
                 print("Game over!")
-                raise SystemExitwwwwwwwwwwwww
+                sys.exit()
+
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    shot.kill()
+                    asteroid.kill()
+
+        screen.fill("black")
 
         for item in drawable:    
             item.draw(screen)
